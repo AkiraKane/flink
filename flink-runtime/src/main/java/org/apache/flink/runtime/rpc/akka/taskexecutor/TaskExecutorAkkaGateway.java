97dbbc85e26df936920dd3c82c2000c7cb596566
Till Rohrmann <trohrmann@apache.org>
2016-08-05 00:27:31 +0200

    Add runAsync method to execute runnables in the main rpc thread

--- flink-runtime/src/main/java/org/apache/flink/runtime/rpc/akka/taskexecutor/TaskExecutorAkkaGateway.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rpc/akka/taskexecutor/TaskExecutorAkkaGateway.java
@@ -25,13 +25,14 @@ import org.apache.flink.runtime.deployment.TaskDeploymentDescriptor;
 import org.apache.flink.runtime.executiongraph.ExecutionAttemptID;
 import org.apache.flink.runtime.messages.Acknowledge;
 import org.apache.flink.runtime.rpc.akka.AkkaGateway;
+import org.apache.flink.runtime.rpc.akka.RunnableAkkaGateway;
 import org.apache.flink.runtime.rpc.akka.messages.CancelTask;
 import org.apache.flink.runtime.rpc.akka.messages.ExecuteTask;
 import org.apache.flink.runtime.rpc.taskexecutor.TaskExecutorGateway;
 import scala.concurrent.Future;
 import scala.reflect.ClassTag$;
 
-public class TaskExecutorAkkaGateway implements TaskExecutorGateway, AkkaGateway {
+public class TaskExecutorAkkaGateway extends RunnableAkkaGateway implements TaskExecutorGateway {
 	private final AskableActorRef actorRef;
 	private final Timeout timeout;
 
